83153fa513e9ccc0d59a7715bb6edc4026ff7b3e
djluo <dj.luo@baoyugame.com>
2018-02-16 20:34:03 +0800

    fix docker bin path

--- jenkins/scripts/docker-build.sh
+++ jenkins/scripts/docker-build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-docker build -t  simple-java .
+/usr/bin/docker build -t  simple-java .
